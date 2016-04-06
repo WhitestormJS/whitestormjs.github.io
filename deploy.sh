a4c7ba300e663e344dedfbf060eb5bdd2d8745e6
sasha240100 <siteprogcom@gmail.com>
2016-04-06 12:36:59 +0300

    Change deploy branch

--- deploy.sh
+++ deploy.sh
@@ -73,7 +73,7 @@ parse_args() {
 
   # Source directory & target branch.
   deploy_directory=build
-  deploy_branch=gh-pages
+  deploy_branch=master
 
   #if no user identity is already set in the current git environment, use this:
   default_username=${GIT_DEPLOY_USERNAME:-deploy.sh}
