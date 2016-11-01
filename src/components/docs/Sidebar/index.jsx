6de24f4a0d274bbc836c4f0553a7c7eaa28f99e5
sasha240100 <alexbuzin88@gmail.com>
2016-11-01 12:09:32 +0200

    Responsive website

--- src/components/docs/Sidebar/index.jsx
+++ src/components/docs/Sidebar/index.jsx
@@ -23,62 +23,66 @@ export default class Sidebar extends Component {
 
     return (
       <div className={this.props.className + ' sidebar'}>
-        <h1>{this.props.title}</h1>
-        <ul>
-          <Match key="main" exactly pattern="/api" children={({matched}) =>
-            <Item link="/api" title="Main page" active={matched} />
-          } />
-
-          {_.map(categories[0], (data, i) => (
-            <Match key={i} pattern={prefix + data[1]} children={({matched}) =>
-              <Item link={prefix + data[1]} title={data[0][1].title}
-                active={matched}
-                icon={data[0][1].icon ? data[0][1].icon : false}
-              />
+        <div className="block">
+          <h1>{this.props.title}</h1>
+          <ul>
+            <Match key="main" exactly pattern="/api" children={({matched}) =>
+              <Item link="/api" title="Main page" active={matched} />
             } />
-          ))}
-        </ul>
 
+            {_.map(categories[0], (data, i) => (
+              <Match key={i} pattern={prefix + data[1]} children={({matched}) =>
+                <Item link={prefix + data[1]} title={data[0][1].title}
+                  active={matched}
+                  icon={data[0][1].icon ? data[0][1].icon : false}
+                />
+              } />
+            ))}
+          </ul>
+        </div>
+
+        <div className="block">
           <h1>API</h1>
 
-        <ul>
-          {_.map(categories[1], (data, key) => {
-            let isActive = false;
+          <ul>
+            {_.map(categories[1], (data, key) => {
+              let isActive = false;
 
-            _.map(data, (page, i) => {
-              isActive = matchPattern(
-                prefix + page[1],
-                window.location,
-                false
-              ) ? true : isActive;
+              _.map(data, (page, i) => {
+                isActive = matchPattern(
+                  prefix + page[1],
+                  window.location,
+                  false
+                ) ? true : isActive;
 
-              isActive = matchPattern(
-                prefix + key + '/' + page[1],
-                window.location,
-                false
-              ) ? true : isActive;
-            });
+                isActive = matchPattern(
+                  prefix + key + '/' + page[1],
+                  window.location,
+                  false
+                ) ? true : isActive;
+              });
 
-            return (
-              <Category
-                title={key}
-                icon={this.props.categories[key] ? this.props.categories[key].icon : false}
-                key={key}
-                id={key}
-                isActive={isActive}
-              >
-                {_.map(data, (page, i) => (
-                  <Match key={i} pattern={prefix + key + '/' + page[1]} children={({matched}) =>
-                    <Item link={prefix + key + '/' + page[1]} title={page[0][1].title}
-                      active={matched}
-                      icon={this.props.categories[key].noExtend ? page[0][1].icon : this.props.categories[key].icon}
-                    />
-                  } />
-                ))}
-              </Category>
-            )
-          })}
-        </ul>
+              return (
+                <Category
+                  title={key}
+                  icon={this.props.categories[key] ? this.props.categories[key].icon : false}
+                  key={key}
+                  id={key}
+                  isActive={isActive}
+                >
+                  {_.map(data, (page, i) => (
+                    <Match key={i} pattern={prefix + key + '/' + page[1]} children={({matched}) =>
+                      <Item link={prefix + key + '/' + page[1]} title={page[0][1].title}
+                        active={matched}
+                        icon={this.props.categories[key].noExtend ? page[0][1].icon : this.props.categories[key].icon}
+                      />
+                    } />
+                  ))}
+                </Category>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     )
   }
